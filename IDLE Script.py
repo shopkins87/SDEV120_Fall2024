010baaef73bbe15ab6bad4510ef69937b543791d
Stephanie Hopkins <stweeks52@gmail.com>
2024-09-30 21:41:10 -0400

    Add files via upload
    Mod 6 IDLE script: The beginning of ParcelTongue

---
+++ IDLE Script.py
@@ -0,0 +1,5 @@
+"""This code will set the 'c' variable and 'd' variable, then print a message""" #Now that I speak parceltongue, I guess I belong in Slytherin now.
+c = "cats" #first variable
+d = "dogs" #second variable
+s = print("It is raining " + c + " and " + d + ".")
+(s)
